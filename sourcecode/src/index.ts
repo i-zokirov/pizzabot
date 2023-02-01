f8c3bd8f3f6d62b8cf1398bc4a928d225aea19c6
i-zokirov <izokirov@outlook.com>
2023-02-01 13:39:58 +0100

    changed dialogflow credentials file path in prod

--- sourcecode/src/index.ts
+++ sourcecode/src/index.ts
@@ -1,4 +1,9 @@
 import "./express";
 import "./bot/index";
 import "./database/index";
-process.env.GOOGLE_APPLICATION_CREDENTIALS = "./src/keys/dialogflow.json";
+
+if (process.env.NODE_ENV === "production") {
+    process.env.GOOGLE_APPLICATION_CREDENTIALS = "/etc/secrets/dialogflow.json";
+} else {
+    process.env.GOOGLE_APPLICATION_CREDENTIALS = "./src/keys/dialogflow.json";
+}
